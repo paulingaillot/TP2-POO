4414cbc1addbeb916da75b56d518df9d6e7faf6f
Hairost <hairost@hotmail.fr>
2022-11-12 08:52:05 -0500

    formatage code

--- Exo4/traitement_donnees/Partie2.py
+++ Exo4/traitement_donnees/Partie2.py
@@ -1,7 +1,6 @@
 import csv
 
 
-
 class Patient:
     age = 0
     genre = 1
@@ -19,10 +18,22 @@ class Patient:
         else:
             return False
     
+    def __init__ (self, patient):
+        self.age = int(patient[0])
+        self.genre = int(patient[1])
+        self.yos = int(patient[2])
+        self.cpd = int(patient[3])
+        self.avg_FEV = float(patient[4])
+        self.FEV = float(patient[5])
+        self.OX2 = float(patient[6])
+        self.Miles = float(patient[7])
+        self.Label = int(patient[8])
+           
+
     def __str__(self):
         return "[" + str(self.age) + ", " + str(self.genre) + ", "  + str(self.yos) + ", "  + str(self.cpd) + ", "  + str(self.avg_FEV) + ", "  + str(self.FEV) + ", "  + str(self.OX2) + ", "  + str(self.Miles) + ", "  + str(self.Label) + "]"
 
-listePatients = [Patient()]
+listePatients = []
 
 def chercherLabel(listePatients, label):
     cpt = 0
@@ -43,8 +54,9 @@ def getAverageAge():
             j = j+1
             total += patient.age
 
-    moyenne = total / j
-    print("Age moyen : "+str(int(moyenne))+" ans.")
+    if j > 0:
+        moyenne = total / j
+        print("Age moyen : "+str(int(moyenne))+" ans.")
 
 
 def getPartMen():
@@ -55,10 +67,11 @@ def getPartMen():
             total = total + 1
             if patient.genre == 1:
                 homme = homme + 1
-    partHomme = (homme*100) / total
-    partFemme = 100-int(partHomme)
-    print("Pourcentage d'Hommes atteind par le type 4 : "+str(int(partHomme))+"%.")
-    print("Pourcentage de Femmes atteind par le type 4 : "+str(int(partFemme))+"%.")
+    if total > 0:
+        partHomme = (homme*100) / total
+        partFemme = 100-int(partHomme)
+        print("Pourcentage d'Hommes atteind par le type 4 : "+str(int(partHomme))+"%.")
+        print("Pourcentage de Femmes atteind par le type 4 : "+str(int(partFemme))+"%.")
 
 def getProbaType4() :
     label1234 =0
@@ -70,22 +83,23 @@ def getProbaType4() :
         if patient.Label != 0 : 
             label1234 = label1234 + 1
             if patient.Label == 1 :
-                label1 = label1 +1;
+                label1 = label1 +1
             elif patient.Label == 2 :
-                label2 = label2 +1;
+                label2 = label2 +1
             elif patient.Label == 3 :
-                label3 = label3 +1;
+                label3 = label3 +1
             elif patient.Label == 4 :
-                label4 = label4 +1;
+                label4 = label4 +1
 
-    proba = round(label1 * 100 / label1234,2)
-    print("Probabilité d'avoir la maladie de type 1 : "+str(proba)+"%.")
-    proba = round(label2 * 100 / label1234,2)
-    print("Probabilité d'avoir la maladie de type 2 : "+str(proba)+"%.")
-    proba = round(label3 * 100 / label1234,2)
-    print("Probabilité d'avoir la maladie de type 3 : "+str(proba)+"%.")
-    proba = round(label4 * 100 / label1234,2)
-    print("Probabilité d'avoir la maladie de type 4 : "+str(proba)+"%.")
+    if label1234 > 0:
+        proba = round(label1 * 100 / label1234,2)
+        print("Probabilité d'avoir la maladie de type 1 : "+str(proba)+"%.")
+        proba = round(label2 * 100 / label1234,2)
+        print("Probabilité d'avoir la maladie de type 2 : "+str(proba)+"%.")
+        proba = round(label3 * 100 / label1234,2)
+        print("Probabilité d'avoir la maladie de type 3 : "+str(proba)+"%.")
+        proba = round(label4 * 100 / label1234,2)
+        print("Probabilité d'avoir la maladie de type 4 : "+str(proba)+"%.")
 
 def compareYOS():
     yos1 = 0
@@ -100,14 +114,15 @@ def compareYOS():
             yos1 = yos1 + patient.yos
             tot1 = tot1 + 1
 
-    moy1 = round(yos1 / tot1,2)
-    moy4 = round(yos4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("Yos           |      "+str(moy1)+"     |      "+str(moy4)+"      |   "+value+"   |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(yos1 / tot1,2)
+        moy4 = round(yos4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("Yos           |      "+str(moy1)+"     |      "+str(moy4)+"      |   "+value+"   |")
 
 def compareCPD():
     cpd1 = 0
@@ -122,14 +137,15 @@ def compareCPD():
             cpd1 = cpd1 + patient.cpd
             tot1 = tot1 + 1
 
-    moy1 = round(cpd1 / tot1,2)
-    moy4 = round(cpd4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("CPD           |      "+str(moy1)+"      |      "+str(moy4)+"       |   "+value+"   |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(cpd1 / tot1,2)
+        moy4 = round(cpd4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("CPD           |      "+str(moy1)+"      |      "+str(moy4)+"       |   "+value+"   |")
 
 
 def compareAverageFEV():
@@ -145,14 +161,15 @@ def compareAverageFEV():
             fev1 = fev1 + patient.avg_FEV
             tot1 = tot1 + 1
 
-    moy1 = round(fev1 / tot1,2)
-    moy4 = round(fev4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("Average FEV   |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(fev1 / tot1,2)
+        moy4 = round(fev4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("Average FEV   |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
 
 
 def compareFEV():
@@ -168,14 +185,15 @@ def compareFEV():
             fev1 = fev1 + patient.FEV
             tot1 = tot1 + 1
 
-    moy1 = round(fev1 / tot1,2)
-    moy4 = round(fev4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("FEV           |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(fev1 / tot1,2)
+        moy4 = round(fev4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("FEV           |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
 
 
 def compareOX2():
@@ -191,14 +209,15 @@ def compareOX2():
             ox1 = ox1 + patient.OX2
             tot1 = tot1 + 1
 
-    moy1 = round(ox1 / tot1,2)
-    moy4 = round(ox4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("Average OX2   |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(ox1 / tot1,2)
+        moy4 = round(ox4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("Average OX2   |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
 
 
 def compareMiles():
@@ -214,14 +233,15 @@ def compareMiles():
             miles1 = miles1 + patient.Miles
             tot1 = tot1 + 1
 
-    moy1 = round(miles1 / tot1,2)
-    moy4 = round(miles4 / tot4,2)
-    value = "correct"
-    if moy1 > moy4 :
-        value = "lack"
-    elif moy1 < moy4 : 
-        value = "excess"
-    print("Average Miles |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
+    if tot1 > 0 & tot4 > 0:
+        moy1 = round(miles1 / tot1,2)
+        moy4 = round(miles4 / tot4,2)
+        value = "correct"
+        if moy1 > moy4 :
+            value = "lack"
+        elif moy1 < moy4 : 
+            value = "excess"
+        print("Average Miles |      "+str(moy1)+"      |      "+str(moy4)+"       |    "+value+"    |")
 
 
 def main():
@@ -234,23 +254,9 @@ def main():
         liste = list(lecture)
 
         # on crée une liste d'objets Patient
-        i = 0
-        for patient in liste:
-            if i == 0:
-                i = i+1
-            else:
-                listePatients.insert(i, Patient())
-                listePatients[i].age = int(patient[0])
-                listePatients[i].genre = int(patient[1])
-                listePatients[i].yos = int(patient[2])
-                listePatients[i].cpd = int(patient[3])
-                listePatients[i].avg_FEV = float(patient[4])
-                listePatients[i].FEV = float(patient[5])
-                listePatients[i].OX2 = float(patient[6])
-                listePatients[i].Miles = float(patient[7])
-                listePatients[i].Label = int(patient[8])
-                i = i+1
-
+        for i, patient in enumerate(liste):
+            if i != 0:
+                listePatients.insert(i, Patient(patient))
         
         chercherLabel(listePatients, 4)
 
