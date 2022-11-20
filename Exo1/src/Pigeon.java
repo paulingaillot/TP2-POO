0195032b5586eca98d75b28fb36e7ff115276cd7
Hairost <hairost@hotmail.fr>
2022-11-20 16:35:16 -0500

    Clean fichier après merge

--- Exo1/src/Pigeon.java
+++ Exo1/src/Pigeon.java
@@ -33,7 +33,18 @@ public class Pigeon extends Thread {
 				} else {
 					sleep(1000);
 
-<<<<<<< HEAD
+					// Le pigeon peut prendre peur
+
+					int aleapeur = 0 + (int) (Math.random() * ((25 - 0) + 1));
+					if (aleapeur == 1) {
+						// Le pigeon prend peur
+						System.out.println("Le pigeon a pris peur");
+						System.out.println("Suppression");
+						this.stopThread();
+					}
+
+					// On verifie l'existence de nourriture
+
 					int min = -1;
 					int direction = -1;
 					for (int i = 0; i < 10; i++) {
@@ -43,43 +54,9 @@ public class Pigeon extends Thread {
 							direction = i;
 						}
 					}
-=======
-            while (blinker == thisThread) {
-                try {
-                    if(isEat == true) {
-                        System.out.println("Suppression");
-                        this.stopThread();
-                    }else {
-                    sleep(1000);
-                    
-                    // Le pigeon peut prendre peur 
 
-                    int aleapeur = 0 + (int)(Math.random() * ((25 - 0) + 1));
-                    if(aleapeur == 1) {
-                        // Le pigeon prend peur
-
-                        System.out.println("Le pigeon a pris peur");
-                        System.out.println("Suppression");
-                        this.stopThread();
-                    }
-
-                    // On verifei l'existence de nourriture
-
-                    int min = -1;
-                    int direction = -1;
-                    for(int i=0; i<10; i++) {
-                        if(Nourriture.Nourrituretab[i] != null && (Nourriture.Nourrituretab[i].getValeur()<min || min ==-1)) {
-                            min = Nourriture.Nourrituretab[i].getValeur();
-                            direction = i;
-                        }
-                    }
->>>>>>> branch 'master' of https://github.com/paulingaillot/TP2-POO.git
-
-<<<<<<< HEAD
+					// Deplacement
 					if (direction != -1) {
-=======
-                    // Deplacement
->>>>>>> branch 'master' of https://github.com/paulingaillot/TP2-POO.git
 
 						if (direction == this.x) {
 							System.out.println("Je mange ...");
@@ -101,7 +78,7 @@ public class Pigeon extends Thread {
 						}
 						Main.mafenetre.UpdatePigeon();
 
-					} else { // direction == -1
+					} else {
 						System.out.println("Je dors ...(pos=" + this.x + ")");
 					}
 				}
